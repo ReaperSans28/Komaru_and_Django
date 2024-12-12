d26ab4b0049d4f365f9ee77034e22402606c475e
ReaperSans28 <smileuwu321@gmail.com>
2024-12-12 13:25:51 +0300

    hw 24

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
+
 
 def home(request):
     return render(request, "home.html")
@@ -13,4 +14,10 @@ def contacts(request):
 def products_list(request):
     products = Product.objects.all()
     context = {"products": products}
-    return render(request, 'base.html', context)
+    return render(request, 'products_list.html', context)
+
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'products_detail.html', context)
