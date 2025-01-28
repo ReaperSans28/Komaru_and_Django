38a2309cf17a1fd182d1dd213ddb8dd7e1d14798
192117 <noti1996@gmail.com>
2025-01-28 23:10:36 +0300

    refactor: add catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from catalog.models import Product, Category
 
 
-@admin.register(Product)
-class KomaruAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "category")
-    list_filter = ("category",)
-    search_fields = ("name", "description")
-
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
+    list_display = ("id", "name", "description")
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description", "price", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "description")
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,27 +1,13 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import (
-    ProductListView,
-    ProductDetailView,
-    ProductCreateView,
-    ProductUpdateView,
-    ProductDeleteView,
-    profile_view,
-    RegisterView,
-)
+from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="products_list"),
-    path("catalog/<int:pk>/", ProductDetailView.as_view(), name="products_detail"),
-    path("catalog/create", ProductCreateView.as_view(), name="products_create"),
-    path(
-        "catalog/<int:pk>/update", ProductUpdateView.as_view(), name="products_update"
-    ),
-    path(
-        "catalog/<int:pk>/delete", ProductDeleteView.as_view(), name="products_delete"
-    ),
-    path("profile", profile_view, name="profile"),
-    path("register", RegisterView.as_view(), name="register"),
+    path("catalogs/<int:pk>/", ProductDetailView.as_view(), name="products_detail"),
+    path("catalogs/create", ProductCreateView.as_view(), name="products_create"),
+    path("catalogs/<int:pk>/update", ProductUpdateView.as_view(), name="products_update"),
+    path("catalogs/<int:pk>/delete", ProductDeleteView.as_view(), name="products_delete"),
 ]
